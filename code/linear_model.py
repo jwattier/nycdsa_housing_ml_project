fe50d3e6c17ff78893771ed76e3b7afc20523aaf
Jason Wattier <jasonwattier@gmail.com>
2020-03-01 14:27:56 -0600

    small updates to linear_model.py and pre-process.ipynb as part of jupytext sync

--- code/linear_model.py
+++ code/linear_model.py
@@ -13,13 +13,11 @@
 #     name: python37664bitbaseconda78814975a87e45dd93a41087a924c115
 # ---
 
-# + pycharm={"is_executing": false}
 import pandas as pd
 # %pylab inline
 
-# + pycharm={"is_executing": false}
-file_path = r"../pre_processed_data/pre_processed.csv"
-
+# file_path = r"../pre_processed_data/pre_processed.csv"
+file_path = r"C:\Users\jason\OneDrive\Documents\Jason\NYC Data Science Academy\projects\machine_learning\ghub_work_area\pre_processed_data\pre_processed.csv"
 # import pre_processed file 
 pre_process_df = pd.read_csv(filepath_or_buffer=file_path, index_col=0, header=0)
 pre_process_df.sample(5)
@@ -27,7 +25,7 @@ pre_process_df.sample(5)
 # + pycharm={"is_executing": false}
 len(pre_process_df.columns)
 
-# + pycharm={"is_executing": false}
+# +
 # remove target variables from data frame
 X = pre_process_df.loc[:, pre_process_df.columns.difference(["saleprice", "log_saleprice"])]
 
@@ -38,7 +36,7 @@ print(X.shape)
 print(y.shape)
 y_log.shape
 
-# + pycharm={"is_executing": false}
+# +
 # train, test split with train set to 80%
 # A linear regression model will be evaluated first in the absence of regularization
 from sklearn.model_selection import train_test_split
@@ -46,7 +44,7 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state=42)
 X_train_log, X_test_log, y_train_log, y_test_log = train_test_split(X, y_log, test_size= 0.2, random_state=42)
 
-# + pycharm={"is_executing": false}
+# +
 from sklearn.linear_model import HuberRegressor, LinearRegression
 # Evaluation of Huber regressor against SalePrice w/o log-transform 
 # Huber regression is a linear model that is more robust to outliers than the standard model, which penalizes the model
@@ -56,16 +54,15 @@ hr = HuberRegressor()
 hr.fit(X=X_train, y=y_train)
 print(f"Train R2 is {hr.score(X=X_train, y=y_train)}")
 print(f"Test R2 is {hr.score(X=X_test, y=y_test)}")
+# -
 
-
-# + pycharm={"is_executing": false}
 # Standard regression w/o log transform 
 lr = LinearRegression()
 lr.fit(X=X_train, y=y_train)
 print(f"Train R2 is {lr.score(X=X_train, y=y_train)}")
 print(f"Test R2 is {lr.score(X=X_test, y=y_test)}")
 
-# + pycharm={"is_executing": false}
+# +
 # Standard regression w/log transform 
 lr_log = LinearRegression()
 lr_log.fit(X=X_train_log, y=y_train_log)
@@ -78,8 +75,8 @@ print(f"Test R2 is {lr_log.score(X=X_test_log, y=y_test_log)}")
 # ## Model Evaluation - Linear Regression
 # ### The following section evaluates the random error, constant variance and normal distribution with mean 0 assumption of linear model in the context of the four initial models utilizing a residual plot from Yellowbrick.
 #
+# -
 
-# + pycharm={"is_executing": false}
 # Residual Plot for Huber LR with no log-transform
 from yellowbrick.regressor import ResidualsPlot
 rpv_hr = ResidualsPlot(hr)
@@ -87,16 +84,12 @@ rpv_hr.fit(X=X_train, y=y_train)
 rpv_hr.score(X=X_test, y=y_test)
 rpv_hr.poof()
 
-
-# + pycharm={"is_executing": false}
 rpv_lr = ResidualsPlot(lr)
 rpv_lr.fit(X=X_train, y=y_train)
 rpv_lr.score(X=X_test, y=y_test)
 rpv_lr.poof()
 
 
-
-# + pycharm={"is_executing": false}
 # Residual Plot for LR with log transform 
 rpv_lr_log = ResidualsPlot(lr_log)
 rpv_lr_log.fit(X=X_train_log, y=y_train_log)
@@ -106,8 +99,8 @@ rpv_lr_log.poof()
 # + [markdown] pycharm={"name": "#%% md\n"}
 # ## Model Evaluation of Ordinary Least Squares -Log Transform
 # - Evaluation of log-transformed OLS model as the residuals plot appeared to satisfy most of the principal assumptions of linear regression. 
+# -
 
-# + pycharm={"is_executing": false}
 import statsmodels.api as sm
 X_add_constant = sm.add_constant(X_train_log)
 ols_log = sm.OLS(y_train_log, X_add_constant)
@@ -121,7 +114,7 @@ print(ans_log.summary())
 # - homeage is being excluded in favor of remodelage due to its lower significance. 
 # - garagecars is being dropped because there is an overalp between that variable and the "two_plus_cr_garg" feature
 
-# + pycharm={"is_executing": false}
+# +
 X_train_log = X_train_log.loc[:, X.columns.difference(["bedroomsabvgr", "lotfrontage", "newHome", "homeage", "garagecars"])]
 X_test_log = X_test_log.loc[:, X.columns.difference(["bedroomsabvgr", "lotfrontage", "newHome", "homeage", "garagecars"])]
 
@@ -135,24 +128,21 @@ print(f"Test R2 is {lr_log.score(X=X_test_log, y=y_test_log)}")
 # - The train and test R2 are very similar to the model (e.g., 90/89%) prior to dropping the four variables. 
 # - The residual plot and stats model output will be evaluated to confirm that the prior assumptions still hold as well as to identify any other items to potentiall exclude before proceeding to cross-validation.
 #
+# -
 
-# + pycharm={"is_executing": false}
 rpv_lr_log = ResidualsPlot(lr_log)
 rpv_lr_log.fit(X=X_train_log, y=y_train_log)
 rpv_lr_log.score(X=X_test_log, y=y_test_log)
 rpv_lr_log.poof()
 
-# + pycharm={"is_executing": false}
 X_add_constant = sm.add_constant(X_train_log)
 ols_log = sm.OLS(y_train_log, X_add_constant)
 ans_log = ols_log.fit()
 print(ans_log.summary())
 
-# -
 
 # - Evaluation of OLS for non-log LR
 
-# + pycharm={"is_executing": false}
 X_add_constant_non_log = sm.add_constant(X_train)
 ols = sm.OLS(y_train, X_add_constant_non_log)
 ans = ols.fit()
@@ -175,9 +165,10 @@ cd_visualizer = cooks_distance(X=X_train, y=y_train_log)
 
 # + [markdown] pycharm={"name": "#%% md\n"}
 # ## Cross Validation through YellowBrick
+# ### **Evaluation of R2 over 4-fold Cross-Validation**
 # - linear log model is evaluated via 4-k fold
 
-# + pycharm={"is_executing": false}
+# +
 from sklearn.model_selection import KFold
 
 from yellowbrick.model_selection import CVScores
@@ -194,11 +185,17 @@ cv_visualizer.poof()
 # - Median cross-validation R2 score is 89% and fairly consistent. 
 # - Evaluating next via sci-kit learn's model selection package
 #
+# -
 
-# + pycharm={"is_executing": false}
+# ### **Evaluation of RMSE (Root Mean Square Error)**
+
+# +
 from sklearn.model_selection import cross_val_score
 lr_r2_scores = cross_val_score(estimator = lr_log, X = X_train_log, y = y_train_log, scoring = 'r2', cv= 4)
-lr_rmse = -1 * cross_val_score(estimator= lr_log, X = X_train_log, y = y_train_log, scoring = "neg_mean_squared_error", cv=4)
+lr_rmse = -1 * cross_val_score(estimator= lr_log, X = X_train_log, y = y_train_log, scoring = "neg_root_mean_squared_error", cv=4)
+lr_mse = -1 * cross_val_score(estimator= lr_log, X = X_train_log, y = y_train_log, scoring = "neg_mean_squared_error", cv=4)
+lr_mae = -1 * cross_val_score(estimator= lr_log, X = X_train_log, y = y_train_log, scoring = "neg_median_absolute_error", cv=4)
+
 def display_cv_scores(scores):
     print("Scores:", scores)
     print("Mean:", scores.mean())
@@ -209,6 +206,24 @@ display_cv_scores(lr_r2_scores)
 print("")
 print("CV scores for RMSE are:")
 display_cv_scores(lr_rmse)
+print("")
+print("CV scores for MSE are:")
+display_cv_scores(lr_mse)
+print("")
+print("CV scores for MAE are:")
+display_cv_scores(lr_mae)
+
+# +
+cv_rmse = KFold(n_splits=4, random_state=42)
+
+# note that scikit-learn's implementation of RMSE is negative root mean squared error 
+cv_visualizer_rmse = CVScores(model=lr_log, cv=cv, scoring="neg_median_absolute_error")
+
+cv_visualizer_rmse.fit(X=X_train_log, y=y_train_log) # fit data into visualizer 
+cv_visualizer_rmse.poof()
+# -
+
+
 
 # + [markdown] pycharm={"name": "#%% md\n"}
 # - Based upon cross-validation and test R2, we appear to have a strong and consistent predictor of housing prices.
@@ -243,19 +258,158 @@ vif.round(1)
 # ## Model Output as Picket Object
 # - Output of model object to be utilized in prediction
 
-# + pycharm={"is_executing": false}
+# +
 from joblib import dump
 
-dump(lr_log, '../model_files/lr_log_model.joblib')
+#dump(lr_log, '../model_files/lr_log_model.joblib')
+dump(lr_log, r"C:\Users\jason\OneDrive\Documents\Jason\NYC Data Science Academy\projects\machine_learning\ghub_work_area\model_files\lr_log_model.joblib")
+# -
 
-# + pycharm={"name": "#%%\n", "is_executing": false}
 lr_log.predict(X_test_log)
 
 
-
-# + pycharm={"name": "#%%\n", "is_executing": false}
 X_test_log.columns
 
-# + pycharm={"name": "#%%\n"}
+# + [markdown] pycharm={"name": "#%%\n"}
+# # Additonal Model Evaluation 
+# ## **Feature Importance**
+# ### 1. Lasso Evaluation
+# - Relatively and on a holistic level, seeing which feature coefficients are "pushed" to zero via a Lasso Regularization can indicate the relative importance of those features to a model. 
+# - To utilize a regularization method - even for evaluation purposes - it is general good practice to standardize the input variables first prior to fitting the model.
+# -
+
+from sklearn import preprocessing
+std = preprocessing.StandardScaler()
+X_train_log_std = std.fit_transform(X_train_log)
+
+# +
+from sklearn import linear_model
+lasso_model = linear_model.LassoLarsCV(
+    cv=4, max_n_alphas=10
+).fit(X_train_log_std, y_train_log)
+fig, ax = plt.subplots(figsize=(12, 8))
+cm = iter(
+    plt.get_cmap("tab20")(
+        np.linspace(0, 1, X_train_log_std.shape[1])
+    )
+)
+for i in range(X_train_log_std.shape[1]):
+    c = next(cm)
+    ax.plot(
+        lasso_model.alphas_,
+        lasso_model.coef_path_.T[:, i],
+        c=c,
+        alpha=0.8,
+        label=X_train_log.columns[i],
+    )
+
+ax.axvline(
+    lasso_model.alpha_,
+    linestyle="-",
+    c="k",
+    label="alphaCV",
+)
+
+plt.ylabel("Regression Coefficients")
+ax.legend(X_train_log.columns, bbox_to_anchor=(1,1))
+plt.xlabel("alpha")
+plt.title(
+    "Regression Coefficients Progression for Lasso Paths"
+)
+fig.savefig(
+    "mlpr_lasso_w_bsmt_adj.png",
+    dpi=300,
+    bbox_inches="tight",
+)
+# -
+
+# - The visualization of the Lasso regularization with increasing alphas indicates that the most significant factors are building square feet, adjusted overall quality, good amenities count, total baths and bad amenities count.
+
+# ### 2. Mutual Information
+# - Scikit learn's mutual information score for regressions helps to quantifies the amount of information gained by including the feature listed. 
+# - The value is bounded between 0 and 1. If the value is zero, there is no relationship between the target and the feature.  
+
+# +
+from sklearn import feature_selection
+
+mic = feature_selection.mutual_info_regression(
+    X_train_log, y_train_log
+)
+
+fig, ax = plt.subplots(figsize=(10, 8))
+(
+    pd.DataFrame(
+        {"feature": X_train_log.columns, "vimp": mic}
+    )
+    .set_index("feature")
+    .plot.barh(ax=ax)
+)
+fig.savefig("mutual_info.png")
+# -
+
+# **Conclusions**
+# - This visualization largely confirms the same relatively most important features (e.g., square footage, overall quality) that the Lasso visualization conferred. It does appear to note that abnormal sales and single family home are not signficant factors, which a little harder to visually distill in the Lassso chart.
+
+# ### 3. SHAP (SHaply Additive exPlanations)
+# - The SHAP library for Python has several nice visualizations that help to inform how predictors influence the output of the model.
+# - The below summary chart helps us to see the global effect of features on the target log sales price. For example, high values of building sqft increase the model output and lower values by contrast lower the target variable.
+
+# +
+import shap
+shap.initjs()
+
+exp = shap.LinearExplainer(model = lr_log, data=X_train_log)
+vals = exp.shap_values(X_train_log)
+fig, ax = plt.subplots(figsize=(6, 4))
+shap.summary_plot(vals, X_train_log)
+fig.savefig(
+    "shap_summary_plt_reduced.png",
+    bbox_inches="tight",
+    dpi=300,
+)
+# -
+
+# - Another method of more narrowly looking at overall feature importance versus inspection of how different value of a feature impact the model, which reflects the mean absoluate SHAP value. 
+
+shap.summary_plot(vals, X_train_log, plot_type="bar")
+
+# ** Conclusion **
+# - Similar to the Lasso and mutual information graphs - the SHAP summary plot of feature importance highlights the same top factors in terms of overall impact (sq footage, overall quality, total baths, good amentity count).
+
+# # Example Predictions (For Purposes of Presentation)
+
+# +
+y_test_log_predict = lr_log.predict(X_test_log)
+y_test_predict_Series = pd.DataFrame(data=y_test_log_predict, index=y_test_log.index, columns=["log_saleprice_predict"])
+
+test_set_w_predict = pd.concat([X_test_log, y_test_log, y_test_predict_Series], axis="columns")
+# -
+
+test_set_w_predict.sample(5)
+
+test_set_w_predict["saleprice"] = np.exp(test_set_w_predict["log_saleprice"])
+test_set_w_predict["saleprice_predict"] = np.exp(test_set_w_predict["log_saleprice_predict"])
+test_set_w_predict["residual"] = test_set_w_predict.saleprice - test_set_w_predict.saleprice_predict
+
+
+test_set_w_predict.sample(5)
+
+file_path_train = r"C:\Users\jason\OneDrive\Documents\Jason\NYC Data Science Academy\projects\machine_learning\ghub_work_area\data\train.csv"
+train_df = pd.read_csv(file_path_train, header=0, index_col=0)
+train_df.loc[[1095, 68, 1216, 531, 1290], ["YrSold", "YearRemodAdd", "YearBuilt"]]
+
+test_set_w_predict[["residual"]].aggregate([mean, median, np.std])
+
+test_set_w_predict[["residual"]].hist()
+plt.xlabel("$ Dollars")
+plt.ylabel("Count")
+plt.title("Difference in Sale Price: Actual Less Predicted")
+plt.savefig("Histogram_of_Residuals.png")
+
+test_set_w_predict["perc_miss"] = test_set_w_predict["residual"]/test_set_w_predict["saleprice"]
+
+test_set_w_predict["perc_miss"].abs().median()
+
+test_set_w_predict["perc_miss"].abs().mean()
 
 
